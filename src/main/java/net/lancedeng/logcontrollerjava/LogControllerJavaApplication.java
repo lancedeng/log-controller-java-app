a00749fa0818801c8a71999fc734b49e87c52c2e
lancedeng <lancedeng@gmail.com>
2022-09-05 16:22:09 +0800

    Initialization for log control sample application in Java

---
+++ src/main/java/net/lancedeng/logcontrollerjava/LogControllerJavaApplication.java
@@ -0,0 +1,13 @@
+package net.lancedeng.logcontrollerjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogControllerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogControllerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/lancedeng/logcontrollerjava/LogControllerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lancedeng.logcontrollerjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogControllerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
